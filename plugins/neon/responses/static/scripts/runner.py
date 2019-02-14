d073ec0b310912ba9f6a05017b72029f8d1b8c13
riniyar8 <riniyar8@gmail.com>
2019-02-14 22:54:34 +0300

    add new script runner in StaticResponse

--- plugins/neon/responses/static/scripts/runner.py
+++ plugins/neon/responses/static/scripts/runner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from traceback import format_exc as Trace
 from ......base import Plugin
 
 
@@ -9,9 +8,10 @@ class ScriptRunner(Plugin):
         self.text = text if isinstance(text, str) else text.decode()
         self.scripts_info = [
             ('<!--#@', '#@-->', self._run_1_0),
+            ('<!--##', '##-->', self._run_1_1),
         ]
 
-    def _run_1_0(self, text, args):
+    def _run_1_1(self, text, args):
         text = '\n'.join(filter(lambda x: len(x) > 0 and not x.isspace(), text.split('\n')))
         local = {'result': ''}
         try:
@@ -19,7 +19,15 @@ class ScriptRunner(Plugin):
             return str(local['result'])
         except Exception as e:
             self.Error('Unexpectedly got: {}', e)
-            self.Debug('Unexpectedly got: {}', Trace())
+            self.Trace('Unexpectedly got: {}', _type='debug')
+
+    def _run_1_0(self, text, args):
+        text = '\n'.join(filter(lambda x: len(x) > 0 and not x.isspace(), text.split('\n')))
+        try:
+            return str(eval(text, args, {}))
+        except Exception as e:
+            self.Error('Unexpectedly got: {}', e)
+            self.Trace('Unexpectedly got: {}', _type='debug')
 
     def run(self, args):
         text = self.text
@@ -32,7 +40,7 @@ class ScriptRunner(Plugin):
                 pt2 = text[j + len(sci[1]):]
                 result = sci[2](text[i + len(sci[0]):j], args)
                 if result is None:
-                    raise RuntimeError('script #{} failed', k)
+                    raise RuntimeError('script №{} failed', k)
                 text = pt1 + result + pt2
                 k += 1
         self.text = text
