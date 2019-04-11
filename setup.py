64c14a24472eae729e25654d9de829c91dd9a3fd
Andrey Komissarov <komissar.off.andrey@gmail.com>
2019-04-11 21:53:27 +0300

    upd README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='kframe',
-    version='3.4',
+    version='3.5',
     author='Komissarov Andrey',
     author_email='kak.to.tam@mail.ru',
     description='Framework for simple web service with it\'s own web-server Neon',
